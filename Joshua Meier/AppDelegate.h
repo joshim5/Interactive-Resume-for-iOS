94b915ee235a21c983158c9fc64f019cf2146812
Joshua Meier <Josh@Macintosh-5.local>
2014-04-09 13:35:30 -0400

    Initial Commit

---
+++ Joshua Meier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Joshua Meier
+//
+//  Created by Joshua Meier on 4/9/14.
+//  Copyright (c) 2014 Joshua Meier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Joshua Meier/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Joshua Meier
+//
+//  Created by Joshua Meier on 4/9/14.
+//  Copyright (c) 2014 Joshua Meier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
